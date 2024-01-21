156cb438f8126d0dfba5cb9041287495c6cd4e7e
Riya Yingran Guo <124668985+riyagooo@users.noreply.github.com>
2024-01-21 10:30:34 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember(){
+    var member = document.getElementById("member");
+    var memberText = document.getElementById("memberText");
+    var memberText2 = document.getElementById("memberText2");
+    member.style.display = "block";
+    memberText.style.display = "none";
+    memberText2.style.display = "none";
+}
